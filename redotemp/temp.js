8253ea6947cbaf8c1badfa0cd088fca47c237a85
sohini062003 <145773628+sohini062003@users.noreply.github.com>
2023-11-09 11:12:34 +0530

    Add files via upload

---
+++ redotemp/temp.js
@@ -0,0 +1,68 @@
+const UIcelsius = document.querySelector('#result-celsius');
+const UIfahrenheit = document.querySelector('#result-fahrenheit');
+const UIkelvin = document.querySelector('#result-kelvin');
+const UIinputValue = document.querySelector('#UIinputValue');
+const UIinputValueUnit = document.querySelector('#UIinputValueUnit');
+
+
+const calculateBtn = document.querySelector('.calculateBtn');
+const temperatureUnit = document.querySelector('#tempUnit');
+
+calculateBtn.addEventListener('click',calculateResult);
+temperatureUnit.addEventListener('click',changeUIUnit);
+
+
+let celsius,fahrenheit,kelvin;
+
+//Calculate function
+function calculateResult(e){
+    let inputNumber = parseInt(UIinputValue.value);
+
+    let optionValue = (temperatureUnit.options[temperatureUnit.selectedIndex].text).toLowerCase();
+
+    if(optionValue == "select your desire temperature unit" || isNaN(inputNumber)){
+       alert('Please Enter Both Number / Temperature Value');
+
+    }else
+        if(optionValue == "celsius"){
+            celsius = inputNumber;
+            fahrenheit = ((inputNumber * 9/5)+32).toFixed(2);
+            kelvin = (inputNumber + 273.15).toFixed(2);
+            UIresultOutput();
+        };
+        if(optionValue == "fahrenheit"){
+            celsius = ((inputNumber-32)*5/9).toFixed(2);
+            fahrenheit = inputNumber;
+            kelvin = (((inputNumber-32)*5/9)+273.15).toFixed(2);
+            UIresultOutput();
+        };
+        if(optionValue == "kelvin"){
+            celsius = (inputNumber - 273.15).toFixed(2);
+            fahrenheit = ((inputNumber - 273.15)*9/5 + 32).toFixed(2);
+            kelvin = inputNumber;
+            UIresultOutput();
+        }
+    e.preventDefault();
+};
+
+function changeUIUnit(){
+    let UIoptionValue = (temperatureUnit.options[temperatureUnit.selectedIndex].text).toLowerCase();
+        if(UIoptionValue == 'celsius'){
+            UIinputValueUnit.innerHTML ='&deg;C';
+        }
+        if(UIoptionValue == 'fahrenheit'){
+            UIinputValueUnit.innerHTML ='&deg;F';
+        }
+        if(UIoptionValue == 'kelvin'){
+            UIinputValueUnit.innerHTML ='&deg;K';
+        }
+}
+
+function UIresultOutput(){
+    UIcelsius.value = celsius;
+    UIfahrenheit.value = fahrenheit;
+    UIkelvin.value = kelvin;
+    UIinputValue.value = '';
+    UIinputValueUnit.value = '';
+
+}
